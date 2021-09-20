2febc667032ece04425e684f7f1d087cf1fe03df
VictorKildahl <victor.petersen2@gmail.com>
2021-09-20 15:40:38 +0200

    it works

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -3,10 +3,9 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { ReactiveComponent } from './reactive/reactive.component';
-import { PasswordDirective } from './password.directive';
 
 @NgModule({
-  declarations: [ReactiveComponent, PasswordDirective],
+  declarations: [ReactiveComponent],
   imports: [CommonModule, ReactiveRoutingModule, ReactiveFormsModule],
 })
 export class ReactiveModule {}
